b68a0d15cb299845a26f84d59f82710159846ef9
Krish Jaiswal <117284024+krishjaiswal09@users.noreply.github.com>
2025-08-22 14:28:33 +0530

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,9 +1,9 @@
 export default {
   PORT: process.env.PORT || 5050,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://krishjaiswal67890:OBK2i9qT4eie1Hv2@cluster0.4bzzc6t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  JWT_SECRET: process.env.JWT_SECRET || 'secretkey123456789',
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '30d',
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  IS_PRODUCTION: process.env.NODE_ENV === 'production',
+  MONGODB_URI: process.env.MONGODB_URI || 
+  JWT_SECRET: process.env.JWT_SECRET ||,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || ,
+  NODE_ENV: process.env.NODE_ENV || ,
+  IS_PRODUCTION: process.env.NODE_ENV === ,
   CORS_ALLOWED_ORIGINS: process.env.CORS_ALLOWED_ORIGINS || '*'
 }; 
